b744f6e799fed3de557cc53a530d19b159eff2b9
Simon <29654835+absolutelysimon@users.noreply.github.com>
2020-05-11 02:33:20 -0600

    Made some wizardry stuff

--- src/App.js
+++ src/App.js
@@ -1,57 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import { Counter } from "./features/counter/Counter";
+import "./App.css";
+import { useDispatch } from "react-redux";
+import { Wizard } from "./wizard/Wizard";
+import { canAdvance } from "./wizard/wizardSlice";
 
 function App() {
+  const dispatch = useDispatch();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
-    </div>
+    <Wizard>
+      <div validation={true}>
+        Step 1
+        <button onClick={() => dispatch(canAdvance())}>Allow Advance</button>
+      </div>
+      <div>Step 2</div>
+      <div forward_label="Finish">Step 3</div>
+      <div>Thanks!</div>
+    </Wizard>
   );
 }
 
--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,10 @@
-import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import { configureStore } from "@reduxjs/toolkit";
+import counterReducer from "../features/counter/counterSlice";
+import wizardReducer from "../wizard/wizardSlice";
 
 export default configureStore({
   reducer: {
     counter: counterReducer,
+    wizard: wizardReducer,
   },
 });
